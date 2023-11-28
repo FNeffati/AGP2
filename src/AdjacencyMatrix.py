68ed79e235b477cd341b113fc0be06831e96e939
Jin YuHan <jin.burgess19@ncf.edu>
2023-11-28 17:38:52 -0500

    AGP2

--- src/AdjacencyMatrix.py
+++ src/AdjacencyMatrix.py
@@ -1,6 +1,7 @@
 import numpy as np
+import pandas as pd
 from data_wrangling import df_possible_flights
-
+from pathlib import Path
 
 def create_matrix(df, airports):
     num_airports = len(airports)
@@ -12,9 +13,13 @@ def create_matrix(df, airports):
     for index, row in df.iterrows():
         source_index = airports.index(row["Source Airport"])
         dest_index = airports.index(row["Destination Airport"])
-        adjacency_matrix[source_index][dest_index] = 1
+        adjacency_matrix[source_index][dest_index] = row["Passengers"]
 
     source = [airports.index('JFK'), airports.index('LGA')]
-    sink = airports.index('SFO')
+    sink = [airports.index('SFO')]
+    # filepath = Path('/Users/yuhanburgess/Documents/GitHub/AGP2/dataset/adjacency_matrix.csv')
+    # filepath.parent.mkdir(parents=True, exist_ok=True)
+    # adjacency_df = pd.DataFrame(adjacency_matrix, columns=airport_ids, index=airport_ids)
 
+    # adjacency_df.to_csv(filepath)
     return adjacency_matrix, source, sink
