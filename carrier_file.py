ae74ca61a78b0f592db7414e5af88f658e11ce06
Jin YuHan <jin.burgess19@ncf.edu>
2023-12-10 12:13:39 -0500

    AGP2

--- carrier_file.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-plane_data = pd.read_csv('/Users/yuhanburgess/Documents/GitHub/AGP2/dataset/planes.dat.txt', sep=',', header=None)
-plane_data = plane_data.rename(columns={0: 'Airline_Name', 1: 'Airline_IATA', 2: 'Airline_ICAO'})
-plane_data['Airline_IATA'] = plane_data.Airline_IATA.astype(str)
-
-
-
